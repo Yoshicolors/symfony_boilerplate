b5b8ec2407195dd1db93f7b7a39456dc88c9ec23
Brodier Baptiste <battiston.brodier@gmail.com>
2025-09-12 17:26:46 +0200

    Exercice 1

--- src/Controller/BurgerController.php
+++ src/Controller/BurgerController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class BurgerController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(): Response
+    #[Route('/burgers', name: 'burgers')]
+    public function list(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('burgers_list.html.twig');
     }
 }
