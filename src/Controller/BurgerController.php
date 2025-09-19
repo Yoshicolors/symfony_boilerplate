baf7e3f986baf0e2f44575a7ad387623fdaa0384
Brodier Baptiste <battiston.brodier@gmail.com>
2025-09-19 16:08:42 +0200

    Fin exercice 1

--- src/Controller/BurgerController.php
+++ src/Controller/BurgerController.php
@@ -13,4 +13,29 @@ class BurgerController extends AbstractController
     {
         return $this->render('burgers_list.html.twig');
     }
+    #[Route('/burgers/{id}', name: 'burgers_show')]
+    public function show(int $id): Response
+    {
+        
+        $burgers = [
+            1 => ['name' => 'Le Classique', 'description' => 'Boeuf, salade, tomate, sauce maison.'],
+            2 => ['name' => 'Le Fromager', 'description' => 'Boeuf, cheddar fondant, oignons caramélisés.'],
+            3 => ['name' => 'Le Veggie', 'description' => 'Galette de légumes, avocat, sauce au yaourt.'],
+        ];
+
+        
+        if (!isset($burgers[$id])) {
+           
+            throw $this->createNotFoundException('Burger non trouvé');
+        }
+
+        
+        $burger = $burgers[$id];
+
+        
+        return $this->render('burgers_show.html.twig', [
+            'id' => $id,
+            'burger' => $burger,
+        ]);
+    }
 }
